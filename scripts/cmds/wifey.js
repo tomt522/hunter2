140b9bca79cba16d07410addf7126e931e88b9d2
ewrsaif571 <saifewr6@gmail.com>
2025-03-04 22:08:13 +0600

    Create wifey.js

---
+++ scripts/cmds/wifey.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  config: {
+    name: "wifey",
+    aliases: ["wifey"],
+    author: "Kshitiz",
+    version: "1.0",
+    cooldowns: 10,
+    role: 0,
+    shortDescription: "Get random wifey ",
+    longDescription: "Get random wifey video",
+    category: "media",
+    guide: "{p}wifey",
+  },
+
+  onStart: async function ({ api, event, args, message }) {
+    api.setMessageReaction("🐤", event.messageID, (err) => {}, true);
+
+    try {
+      const response = await axios.get(`https://wifey-evzk.onrender.com/kshitiz`, { responseType: "stream" });
+
+      const tempVideoPath = path.join(__dirname, "cache", `${Date.now()}.mp4`);
+
+      const writer = fs.createWriteStream(tempVideoPath);
+      response.data.pipe(writer);
+
+      writer.on("finish", async () => {
+        const stream = fs.createReadStream(tempVideoPath);
+
+        message.reply({
+          body: `Random Wifey Videos🎀😫.`,
+          attachment: stream,
+        });
+
+        api.setMessageReaction("🐍", event.messageID, (err) => {}, true);
+      });
+    } catch (error) {
+      console.error(error);
+      message.reply("Sorry, an error occurred while processing your request.");
+    }
+  }
+};
