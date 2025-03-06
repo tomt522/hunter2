bb4666e420842ca6a3c29dc75a5dc6a8f232893a
ewrsaif571 <saifewr6@gmail.com>
2025-03-06 21:12:26 +0600

    Create flux.js

---
+++ scripts/cmds/flux.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const config = {
+  name: 'flux',
+  category: 'tool',
+  author: 'Romim|nyx',
+  role: 2,
+};
+const fetchImg =  async (prompt, senderID) => {
+  const apiUrl = `https://ljrm5l-3000.csb.app/api/flux?prompt=${encodeURIComponent(prompt)}&uid=61566554077088`;
+  const response = await axios.get(apiUrl);
+  return response.data;
+};
+const onStart = async ({ args, event, message }) => {
+  const query = args.join(' ');
+
+  if (!query) {
+    message.reply('Please provide a prompt🐤');
+  }
+
+  try {
+    const time1 = Date.now();
+    const msg = await message.reply('Please wait...');
+
+    const imageUrl = await fetchImg(query, event.senderID);
+
+    const timeTaken = ((Date.now() - time1 ) / 1000).toFixed(2);
+
+    await message.unsend(msg.messageID);
+    message.reply({
+      body: `Here's your image\nTaken: ${timeTaken} seconds`,
+      attachment: await global.utils.getStreamFromURL(imageUrl),
+    });
+  } catch (error) {
+    message.reply(`Error: ${error.message}`);
+  }
+};
+module.exports = {
+  config,
+  onStart
+                          }
