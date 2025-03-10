57e08a4ca661bc8632b4cb7d68dcf4005d06f4fb
ewrsaif571 <saifewr6@gmail.com>
2025-03-10 11:47:51 +0600

    Update out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -2,29 +2,29 @@ const axios = require("axios");
 const fs = require("fs-extra");
 const request = require("request");
 module.exports = {
-	config: {
-		name: "Out",
-		aliases: ["out"],
-		version: "1.0",
-		author: "Sandy",
-		countDown: 5,
-		role: 2,
-		shortDescription: "bot will leave gc",
-		longDescription: "",
-		category: "admin",
-		guide: {
-			vi: "{pn} [tid,blank]",
-			en: "{pn} [tid,blank]"
-		}
-	},
+        config: {
+                name: "Out",
+                aliases: ["out", " leave"],
+                version: "1.0",
+                author: "Sandy",
+                countDown: 5,
+                role: 1,
+                shortDescription: "bot will leave gc",
+                longDescription: "",
+                category: "admin",
+                guide: {
+                        vi: "{pn} [tid,blank]",
+                        en: "{pn} [tid,blank]"
+                }
+        },
 
-	onStart: async function ({ api,event,args, message }) {
+        onStart: async function ({ api,event,args, message }) {
  var id;
  if (!args.join(" ")) {
  id = event.threadID;
  } else {
  id = parseInt(args.join(" "));
  }
- return api.sendMessage('YOUR TWINKLE LEAVE 😿💔:\n》successfully bot leave_😞👋.\n\n➤ALLAH__HAFEZ 🌷✨', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
-		}
-}
+ return api.sendMessage('bot leave the group', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
+                }
+        };
